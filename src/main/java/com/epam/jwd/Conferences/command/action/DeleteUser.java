01bfcddbace4d244ff940a94ba1b02bf454f3d32
Ihar <ihar.bakhanovich@gmail.com>
2021-08-18 18:09:03 +0300

    DeleteUser: the bug was fixed

--- src/main/java/com/epam/jwd/Conferences/command/action/DeleteUser.java
+++ src/main/java/com/epam/jwd/Conferences/command/action/DeleteUser.java
@@ -53,12 +53,8 @@ public class DeleteUser implements Command {
         final String userRole = String.valueOf(request.getParameter(ApplicationConstants.USER_ROLE_PARAMETER_NAME));
         final Long deleterId = Long.valueOf(request.getParameter(ApplicationConstants.DELETER_ID_PARAMETER_NAME));
         final String deleterRole = String.valueOf(request.getParameter(ApplicationConstants.DELETER_ROLE_PARAMETER_NAME));
-//        final String eMail = String.valueOf(request.getParameter(ApplicationConstants.EMAIL_PARAMETER_NAME));
-//        final String nickname = String.valueOf(request.getParameter(ApplicationConstants.NICKNAME_PARAMETER_NAME));
-//        final String firstName = String.valueOf(request.getParameter(ApplicationConstants.FIRSTNAME_PARAMETER_NAME));
-//        final String surname = String.valueOf(request.getParameter(ApplicationConstants.SURNAME_PARAMETER_NAME));
-//        final Role role = Role.valueOf(request.getParameter(ApplicationConstants.ROLE_PARAMETER_NAME));
         Optional<User> userFromDB = service.findUserByID(userId);
+
         // parameters validation
         if (!validator.isUserWithIdExistInSystem(userId)
                 || !validator.isRoleWithSuchNameExistInSystem(userRole)
@@ -81,66 +77,10 @@ public class DeleteUser implements Command {
 
         final List<User> users = service.findAllUsers();
 
-        // validation whether the user with such id exists in system
-//        if (!validator.isUserWithIdExistInSystem(userId) || !validator.isUserWithNicknameExistInSystem(nickname)) {
-//            return prepareErrorPageBackToMainPage(request, ApplicationConstants.INVALID_ID_NO_SUCH_USER_IN_SYSTEM_MSG);
-//        }
-
-        // validation of the existence of parameters
-//        if (updaterId == null || updaterRole == null || role == null || eMail == null) {
-//            return prepareErrorPageBackToMainPage(request, ApplicationConstants.INVALID_PARAMETERS_MSG);
-//        }
-
         // validation of permission to delete
         if (!deleterId.equals(userId) && !deleterRole.equals(ApplicationConstants.ADMIN_CONSTANT)) {
             return prepareErrorPage(request, ApplicationConstants.NO_PERMISSION_TO_UPDATE_USER_MSG);
         }
-
-        // validation of matching the id and login of user to update
-//        if (!userFromDB.getId().equals(id)) {
-//            return prepareErrorPage(request, ApplicationConstants.USER_TO_UPDATE_DONT_MATCH_WITH_ITS_ID_MSG);
-//        }
-
-        // email validation
-//        if (!validator.isEmailValid(eMail)) {
-//            return prepareErrorPage(request, ApplicationConstants.EMAIL_NOT_VALID_MSG);
-//        }
-
-        // email string validation
-//        if (!validator.isStringValid(eMail)) {
-//            return prepareErrorPage(request, ApplicationConstants.EMAIL_NOT_VALID_UTF8_MSG);
-//        }
-
-        // string validation (firstName)
-//        if (!firstName.trim().equals("")) {
-//            if (!validator.isStringValid(firstName)) {
-//                return prepareErrorPage(request, ApplicationConstants.INVALID_FIRST_NAME_NOT_UTF8_MSG);
-//            }
-//        }
-
-        // string validation (surname)
-//        if (!surname.trim().equals("")) {
-//            if (!validator.isStringValid(surname)) {
-//                return prepareErrorPage(request, ApplicationConstants.INVALID_SURNAME_NOT_UTF8_MSG);
-//            }
-//        }
-
-//        if (!validator.isLengthValid(eMail, ApplicationConstants.MAX_LENGTH_OF_EMAIL_IN_DB)) {
-//            return prepareErrorPage(request, ApplicationConstants.EMAIL_NOT_VALID_TOO_LONG_MSG);
-//        }
-//
-//        if (!validator.isLengthValid(firstName, ApplicationConstants.MAX_LENGTH_OF_FIRST_NAME_IN_DB)) {
-//            return prepareErrorPage(request, ApplicationConstants.INVALID_FIRST_NAME_IS_TOO_LONG_MSG);
-//        }
-//
-//        if (!validator.isLengthValid(surname, ApplicationConstants.MAX_LENGTH_OF_SURNAME_IN_DB)) {
-//            return prepareErrorPage(request, ApplicationConstants.INVALID_SURNAME_TOO_LONG_MSG);
-//        }
-
-//        // check parameter role
-//        if (!validator.isRoleExistInSystem(role)) {
-//            return prepareErrorPageBackToMainPage(request, ApplicationConstants.INVALID_ROLE_PARAMETER_MSG);
-//        }
 
         // check whether the Role 'ADMIN' is deleted
         // Optional<User> userFromDatabase = service.findUserByID(id);
@@ -157,9 +97,14 @@ public class DeleteUser implements Command {
             ) {
                 service.deleteReport(report.getId());
             }
-            final List<User> usersAfterDelete = service.findAllUsers();
-            request.setAttribute(ApplicationConstants.USERS_ATTRIBUTE_NAME, usersAfterDelete);
-            return ApplicationConstants.SHOW_USERS_PAGE_RESPONSE;
+            if (!deleterId.equals(userId)) {
+                final List<User> usersAfterDelete = service.findAllUsers();
+                request.setAttribute(ApplicationConstants.USERS_ATTRIBUTE_NAME, usersAfterDelete);
+                return ApplicationConstants.SHOW_USERS_PAGE_RESPONSE;
+            } else {
+                request.invalidateCurrentSession();
+                return ApplicationConstants.MAIN_PAGE_REDIRECT;
+            }
         } else if (userRoleFromBD.getName().equals(ApplicationConstants.ADMIN_CONSTANT)) {
             return prepareErrorPage(request, ApplicationConstants.NO_PERMISSION_TO_DELETE_ADMIN_MSG);
         }
@@ -205,9 +150,14 @@ public class DeleteUser implements Command {
         ) {
             service.deleteReport(report.getId());
         }
-        final List<User> usersAfterDelete = service.findAllUsers();
-        request.setAttribute(ApplicationConstants.USERS_ATTRIBUTE_NAME, usersAfterDelete);
-        return ApplicationConstants.SHOW_USERS_PAGE_RESPONSE;
+        if (!deleterId.equals(userId)) {
+            final List<User> usersAfterDelete = service.findAllUsers();
+            request.setAttribute(ApplicationConstants.USERS_ATTRIBUTE_NAME, usersAfterDelete);
+            return ApplicationConstants.SHOW_USERS_PAGE_RESPONSE;
+        } else {
+            request.invalidateCurrentSession();
+            return ApplicationConstants.MAIN_PAGE_REDIRECT;
+        }
     }
 
     private CommandResponse prepareErrorPage(CommandRequest request,
